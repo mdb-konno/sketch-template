6857a94fde2cc94edfdc1d69be2ff7d34c0af876
KONNO <k-konno@mediba.jp>
2017-06-20 13:38:11 +0900

    パス変更

--- sh/sketch-to-json.sh
+++ sh/sketch-to-json.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
-if [ $# != 1 ]; then
+if [ $# != 2 ]; then
   echo "Specify the sketch file and then execute."
   exit 1
 fi
 
-fileName=$1
-fileData="./sketch/${fileName}.sketch"
-dirPath="./json/"
-dirName="./raw/"
+outPath=$2
 
 if [ -f ${fileData} ]; then
-  unzip -o ${fileData} -d ${dirPath}${dirName}
+  unzip -o "./Source Files/$1.sketch" -d ${outPath}
   find . -name '*.json' -exec sh -c 'cat {} | jq -rS . > {}.1 && mv {}.1 {}' \;
-  rm -rf ${dirPath}${dirName}/__MACOSX ${dirPath}${dirName}/.DS_Store
-  echo -e "\n\n 🛠 Output! \n\n"
+  rm -rf ${outPath}/__MACOSX ${outPath}/.DS_Store
+  echo -e "\n\n 🛠 Done output! \n\n"
 fi
